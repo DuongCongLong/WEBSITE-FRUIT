663bf66ac073bf930009a7e020f65730e4b44351
CristyLong <168890133+DuongCongLong@users.noreply.github.com>
2026-06-25 19:22:42 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/WEBSITE-FRUIT/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
